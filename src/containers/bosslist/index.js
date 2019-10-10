dee94a6e6088d08eb9c44c395cade0cda7444d50
zhangcx <zhangcx01@mail.jj.cn>
2019-10-10 23:26:16 +0800

    显示列表

--- src/containers/bosslist/index.js
+++ src/containers/bosslist/index.js
@@ -1,9 +1,119 @@
 import React from 'react';
+import { ListView } from 'antd-mobile';
+import BossItem from '../../components/bossitem';
 
+const data = [
+    {
+        img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
+        title: 'Meet hotel',
+        des: '不是所有的兼职汪都需要风吹日晒',
+    },
+    {
+        img: 'https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png',
+        title: 'McDonald\'s invites you',
+        des: '不是所有的兼职汪都需要风吹日晒',
+    },
+    {
+        img: 'https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png',
+        title: 'Eat the week',
+        des: '不是所有的兼职汪都需要风吹日晒',
+    },
+];
+const NUM_SECTIONS = 5;
+const NUM_ROWS_PER_SECTION = 5;
+let pageIndex = 0;
+
+const dataBlobs = {};
+let sectionIDs = [];
+let rowIDs = [];
+function genData(pIndex = 0) {
+    for (let i = 0; i < NUM_SECTIONS; i++) {
+        const ii = (pIndex * NUM_SECTIONS) + i;
+        const sectionName = `Section ${ii}`;
+        sectionIDs.push(sectionName);
+        dataBlobs[sectionName] = sectionName;
+        rowIDs[ii] = [];
+
+        for (let jj = 0; jj < NUM_ROWS_PER_SECTION; jj++) {
+            const rowName = `S${ii}, R${jj}`;
+            rowIDs[ii].push(rowName);
+            dataBlobs[rowName] = rowName;
+        }
+    }
+    sectionIDs = [...sectionIDs];
+    rowIDs = [...rowIDs];
+}
 export default class BossList extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+        const getSectionData = (dataBlob, sectionID) => dataBlob[sectionID];
+        const getRowData = (dataBlob, sectionID, rowID) => dataBlob[rowID];
+
+        const dataSource = new ListView.DataSource({
+            getRowData,
+            getSectionHeaderData: getSectionData,
+            rowHasChanged: (row1, row2) => row1 !== row2,
+            sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+        });
+
+        this.state = {
+            dataSource,
+            isLoading: true,
+            height: document.documentElement.clientHeight * 3 / 4,
+        };
+    }
+
+    componentDidMount() {
+        genData();
+
+        this.setState({
+            dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlobs, sectionIDs, rowIDs),
+            isLoading: false,
+        });
+    }
+
+    renderRow = () => {
         return (
-            <div>BossList</div>
+            <BossItem />
+        )
+    }
+
+    render() {
+        const separator = (sectionID, rowID) => (
+            <div
+                key={`${sectionID}-${rowID}`}
+                style={{
+                    backgroundColor: '#F5F5F9',
+                    height: 8,
+                    borderTop: '1px solid #ECECED',
+                    borderBottom: '1px solid #ECECED',
+                }}
+            />
+        );
+        return (
+            <ListView
+                ref={el => this.lv = el}
+                dataSource={this.state.dataSource}
+                // renderHeader={() => <span>header</span>}
+                renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
+                    {this.state.isLoading ? 'Loading...' : 'Loaded'}
+                </div>)}
+                // renderSectionHeader={sectionData => (
+                //     <div>{`Task ${sectionData.split(' ')[1]}`}</div>
+                // )}
+                // renderBodyComponent={() => <MyBody />}
+                renderRow={this.renderRow}
+                renderSeparator={separator}
+                style={{
+                    height: '100%',
+                    overflow: 'auto',
+                }}
+            //     pageSize={4}
+            //     onScroll={() => { console.log('scroll'); }}
+            // // scrollRenderAheadDistance={500}
+            // // onEndReached={this.onEndReached}
+            // // onEndReachedThreshold={10}
+            />
         )
     }
 }
