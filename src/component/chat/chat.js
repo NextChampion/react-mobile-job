fe1c0ac4359b2fa8665692faae91bc36cf3f343e
zhangcx01 <zhangcx01@mail.jj.cn>
2020-06-21 22:49:30 +0800

    前后端调通socket

--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import io from 'socket.io-client'
 import './chat.css'
 class Chat extends React.Component {
+    componentDidMount() {
+        console.log('发起链接');
+        const socket = io('ws://localhost:9093');
+        console.log(socket);
+
+    }
     render() {
         const { match } = this.props;
 
