bd85e626540586599cfa1fb8c058fae9d4cdc02e
zhangcx01 <zhangcx01@mail.jj.cn>
2020-06-25 00:20:05 +0800

    完成聊天列表的展示

--- src/container/message/message.js
+++ src/container/message/message.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { List } from 'antd-mobile';
+import { Brief } from 'antd-mobile/lib/list/ListItem';
 
+@connect(
+    state => state
+)
 class Message extends React.Component {
-
+    getList = (arr) => {
+        return arr[arr.length -1]
+    }
     render() {
+        const msgGroup = {};
+        const { chat , user} = this.props;
+        const { chatmsg, users } = chat || {};
+        const { _id: mineId } = user || {};
+        let chatList = []
+        if (!chatmsg || !chatmsg.length) {
+            chatList = []
+        }
+        chatmsg.forEach(v => {
+            msgGroup[v.chatid] = msgGroup[v.chatid] || [];
+            msgGroup[v.chatid].push(v);
+        });
+         chatList = Object.values(msgGroup);
         return (
             <div>
                 Message
+                <List>
+                    {chatList.map(v => {
+                        const last = this.getList(v);
+                        const { content, to,from } = last || {};
+                        const targetId = mineId === to ? from : to;
+                        const targetUserInfo = users[targetId] || {};
+                        if (!targetUserInfo) {
+                            return null;
+                        }
+                        const { name, avatar } = targetUserInfo || {};
+                        const icon = require(`../../component/images/${avatar || 'h00001.png'}`);
+                       return (
+                           <List.Item
+                               key={last._id}
+                               thumb={icon}
+                           >
+                               {content}
+                               <Brief>{name|| '对方的名字'}</Brief>
+                           </List.Item>)
+                   })}
+                 </List>
             </div>
         )
     }
